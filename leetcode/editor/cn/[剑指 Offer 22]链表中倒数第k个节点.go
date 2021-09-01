aaa063059ad6e64803b9017bd352ec5914689023
raja <1647193241@qq.com>
2021-09-02 04:35:50 +0800

    leetcode

---
+++ leetcode/editor/cn/[剑指 Offer 22]链表中倒数第k个节点.go
@@ -0,0 +1,48 @@
+//输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+//
+// 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+//
+//
+//
+// 示例：
+//
+//
+//给定一个链表: 1->2->3->4->5, 和 k = 2.
+//
+//返回链表 4->5.
+// Related Topics 链表 双指针 👍 229 👎 0
+package main
+
+func main() {
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func getKthFromEnd(head *ListNode, k int) *ListNode {
+	a, b := head, head
+	for i := 1; i < k; i++ {
+		if b == nil {
+			return nil
+		}
+		b = b.Next
+	}
+	for ; b.Next != nil; b = b.Next {
+		a = a.Next
+	}
+	return a
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
