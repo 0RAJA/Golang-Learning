36ab5ef4ba70a069812cfcb5575ef11fcabdc0fd
raja <1647193241@qq.com>
2021-09-14 18:39:54 +0800

    leetcode

---
+++ leetcode/editor/cn/[524]通过删除字母匹配到字典里最长单词.go
@@ -0,0 +1,65 @@
+//给你一个字符串 s 和一个字符串数组 dictionary 作为字典，找出并返回字典中最长的字符串，该字符串可以通过删除 s 中的某些字符得到。
+//
+// 如果答案不止一个，返回长度最长且字典序最小的字符串。如果答案不存在，则返回空字符串。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "abpcplea", dictionary = ["ale","apple","monkey","plea"]
+//输出："apple"
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "abpcplea", dictionary = ["a","b","c"]
+//输出："a"
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 1000
+// 1 <= dictionary.length <= 1000
+// 1 <= dictionary[i].length <= 1000
+// s 和 dictionary[i] 仅由小写英文字母组成
+//
+// Related Topics 数组 双指针 字符串 排序 👍 167 👎 0
+
+package main
+
+import (
+	"sort"
+)
+
+func main() {
+
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func findLongestWord(s string, dictionary []string) (ret string) {
+	sort.Slice(dictionary, func(i, j int) bool {
+		if len(dictionary[i]) == len(dictionary[j]) {
+			return dictionary[i] < dictionary[j]
+		}
+		return len(dictionary[i]) > len(dictionary[j])
+	})
+	for _, v := range dictionary {
+		i := 0
+		for j := range s {
+			if s[j] == v[i] {
+				i++
+			}
+			if i == len(v) {
+				return v
+			}
+		}
+	}
+	return
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
