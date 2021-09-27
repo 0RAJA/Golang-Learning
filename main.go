16412eb0b8205d2dc9895ba2836189336b2ce2a4
raja <1647193241@qq.com>
2021-09-27 15:55:24 +0800

    leetcode

--- main.go
+++ main.go
@@ -1,51 +1,44 @@
 package main
 
-import "fmt"
-
-var Map [9][9]int  //存储路径
-var book [9][9]int //判断是否走过
-var next = [8][2]int{{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}}
-var x = 1
-var m, n int
-var IsOK bool
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Scan(&m, &n)
-	Map[m][n] = x
-	book[m][n] = 1
-	x++
-	CheckerBored(m, n)
-	print()
+	fmt.Println(numDecodings("**"))
 }
-func CheckerBored(m, n int) {
-	for k := 0; k < 8; k++ {
-		tx := m + next[k][0]
-		ty := n + next[k][1]
-		if tx > 8 || tx < 1 || ty > 8 || ty < 1 { //如果越界就不考虑
-			continue
-		}
-		if book[tx][ty] == 0 { //如果这个地方没有走到，就走这个地方
-			book[tx][ty] = 1 //每次走到一个没走到的地方就标记
-			Map[tx][ty] = x  //记录路径
-			x++
-			CheckerBored(tx, ty) //递归搜索
-			if IsOK {
-				return
+
+const mod = 1e9 + 7
+
+func numDecodings(s string) int {
+	mapCnt := make(map[string]int)
+	for i := 1; i <= 26; i++ {
+		mapCnt[strconv.Itoa(i)] = 1
+	}
+	mapCnt["*"] = 9
+	mapCnt["1*"] = 9
+	mapCnt["2*"] = 6
+	mapCnt["**"] = 15
+	fx := make([]int, len(s)+1)
+	fx[0] = 1
+	for i := 1; i <= len(s); i++ {
+		if i == 1 {
+			fx[i] = mapCnt[s[i-1:i]] % mod
+		} else {
+			var num1 int
+			if s[i-2] == '*' && s[i-1] != '*' {
+				if s[i-1] <= '6' {
+					num1 = 2
+				} else {
+					num1 = 1
+				}
+			} else {
+				num1 = mapCnt[s[i-2:i]]
 			}
-			if x > 64 {
-				IsOK = true
-			}
-			book[tx][ty] = 0 //回溯的时候取消标记
-			Map[tx][ty] = 0
-			x--
+			num2 := mapCnt[s[i-1:i]]
+			fx[i] = (num1*fx[i-2] + num2*fx[i-1]) % mod
 		}
 	}
-}
-func print() {
-	for i := 1; i <= 8; i++ {
-		for j := 1; j <= 8; j++ {
-			fmt.Printf("%d ", Map[i][j])
-		}
-		fmt.Printf("\n")
-	}
+	return fx[len(s)]
 }
