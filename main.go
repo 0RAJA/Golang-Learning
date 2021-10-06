710f38bb239d33ec024ab2b66a23d13e9b442cbd
raja <1647193241@qq.com>
2021-10-06 10:09:11 +0800

    leetcode

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
+	rb := bufio.NewReader(os.Stdin)
+	a, size, err := rb.ReadRune()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(a), size)
 }
