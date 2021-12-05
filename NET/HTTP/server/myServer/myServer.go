90aa4a08e7ed8b79d25fcc8b83763e77d32639cc
raja <1647193241@qq.com>
2021-12-05 10:37:32 +0800

    leetcode

--- NET/HTTP/server/myServer/myServer.go
+++ NET/HTTP/server/myServer/myServer.go
@@ -18,11 +18,11 @@ func main() {
 	//http.Handle("/go", &myHandler)
 	//设置服务器参数
 	server := http.Server{
-		Addr:      ":8080",    // 监听的TCP地址，如果为空字符串会使用":http"
-		TLSConfig: nil,        //可选的TLS配置，用于ListenAndServeTLS方法
+		Addr:      ":8080", // 监听的TCP地址，如果为空字符串会使用":http"
+		TLSConfig: nil,     //可选的TLS配置，用于ListenAndServeTLS方法
 		//ReadTimeout: 2 * time.Second, // 请求的读取操作在超时前的最大持续时间
+		Handler: http.TimeoutHandler(&myHandler, time.Second*2, "服务端操作超时"),
 	}
-	server.Handler = http.TimeoutHandler(&myHandler, time.Second*2, "服务端操作超时")
 	server.ListenAndServe()
 	//http.ListenAndServe(":8080", nil)
 }
